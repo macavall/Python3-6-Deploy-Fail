b8e0001e41fff1e736564dd8708682790c799f56
Matthew Cavallo <macavall@microsoft.com>
2022-09-26 15:19:44 -0500

    First

---
+++ HttpTrigger1/__init__.py
@@ -0,0 +1,31 @@
+import logging
+from math import sqrt
+from joblib import Parallel, delayed
+
+
+import azure.functions as func
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    Parallel(n_jobs=2)(delayed(sqrt)(i ** 2) for i in range(10))
+    [0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0]
+
+
+    if name:
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    else:
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )
